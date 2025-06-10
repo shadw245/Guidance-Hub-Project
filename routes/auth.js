29dda009a171c7352e59f3b67fbae46f4cdd1a70
Shadw Abdelaziz <shadw2405@gmail.com>
2025-06-10 17:29:29 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login, logout, requireAdmin, requireInstructor } = require("../controllers/auth"); // ✅ Grouped imports
+const { signup, login, logout } = require("../controllers/auth"); //  Grouped imports
+const { requireAdmin } = require('../middleware/isAdmin'); // Fixed import
+const { requireInstructor } = require('../middleware/isInstructor'); // Fixed import
 
 router.post("/signup", signup);
 router.post("/login", login);
