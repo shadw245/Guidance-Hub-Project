dfb15d3bf9fe6f82d9655ecf36f1abaf4182247f
seifahmeedd <seifahmed10@outlook.com>
2025-06-14 16:45:46 +0300

    Delete middleware directory

--- middleware/isAdmin.js
+++
@@ -1,7 +0,0 @@
-
-module.exports.requireAdmin = function(req, res, next) {
-    if (req.session && req.session.user && req.session.user.isAdmin) {
-        return next();
-    }
-    res.status(403).send("Access denied. Admins only.");
-};
--- middleware/isInstructor.js
+++
@@ -1,6 +0,0 @@
-module.exports.requireInstructor = function(req, res, next) {
-    if (req.session && req.session.user && req.session.user.isInstructor) {
-        return next();
-    }
-    res.status(403).send("Access denied. Instructors only.");
-};
